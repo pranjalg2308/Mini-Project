168764788534fc408dbe2435c968bff2db19bee3
Pranjal <pranjalg2308@gmail.com>
2018-11-03 22:35:00 +0530

    Basic Class Create

---
+++ src/com/company/Point.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Point {
+    private int pointStatus;
+    private int xCoordinate;
+    private int YCoordinate;
+
+    public int getPointStatus() {
+        return pointStatus;
+    }
+
+    public void setPointStatus(int pointStatus) {
+        this.pointStatus = pointStatus;
+    }
+
+    public int getxCoordinate() {
+        return xCoordinate;
+    }
+
+    public void setxCoordinate(int xCoordinate) {
+        this.xCoordinate = xCoordinate;
+    }
+
+    public int getYCoordinate() {
+        return YCoordinate;
+    }
+
+    public void setYCoordinate(int YCoordinate) {
+        this.YCoordinate = YCoordinate;
+    }
+}
---
+++ src/com/company/Status.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Status {
+    public static final int OBSTACLE = 3;
+    public static final int START = 2;
+    public static final int END = 1;
+    public static final int CLOSED = 0;
+    public static final int LIVE = 4;
+
+}
